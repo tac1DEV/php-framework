6c1461da66bad3e2cc548c2af4c76309098611dc
toto la menace <thomas.sansberro@eemi.com>
2024-11-30 11:58:42 +0100

    Modification de la réponse

--- app/src/Controllers/ContactController.php
+++ app/src/Controllers/ContactController.php
@@ -10,7 +10,7 @@ class ContactController extends AbstractController {
 
     public function process(Request $request): Response {
         if($request->checkValidity() === false){
-            return new Response('Requete invalide');
+            return new Response('Requete invalide. Doit retourner un email, un sujet et un message.',400,[]);
         }
         return new Response('Contact Controller');
     }
