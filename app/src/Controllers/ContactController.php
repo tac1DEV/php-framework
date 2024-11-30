2243e8e8e172644e29485f32334daf0726b3e273
toto la menace <thomas.sansberro@eemi.com>
2024-11-30 17:29:51 +0100

    Nettoyage du merge

--- app/src/Controllers/ContactController.php
+++ app/src/Controllers/ContactController.php
@@ -9,24 +9,6 @@ class ContactController extends AbstractController {
 
 
     public function process(Request $request): Response {
-<<<<<<< HEAD
-        //vérifier le format de la requete
-                    // var_dump($request->getHeaders());
-        
-        $response = new Response();
-        $response->setContent(file_get_contents('php://input'));
-        $response->setStatus(200);
-        $response->setHeaders($request->getHeaders());
-        
-        if($request->getHeaders()["Content-Type"] !== "application/json"){
-            return new Response('Mauvais format. La requête doit etre au format JSON.');
-        }
-        if(!$request->checkValidity()){
-            return new Response('La requête doit avoir les champs "email", "subject" et "message".', 400, $response->getHeaders());
-        } 
-
-        return $response;
-=======
         if($request->checkValidity() === false){
             return new Response('Requete invalide. Doit retourner un email, un sujet et un message.',400,[]);
         }
@@ -49,6 +31,5 @@ class ContactController extends AbstractController {
             echo "Erreur lors de l'enregistrement des données.";
         }
         return new Response('Contact Controller');
->>>>>>> feat/Create-Contact
     }
 }
