17633baee9662a73c03fd52fd9337dd83f71c7a9
toto la menace <thomas.sansberro@eemi.com>
2024-11-30 13:13:29 +0100

    ajout de l'export du contact

--- app/src/Controllers/ContactController.php
+++ app/src/Controllers/ContactController.php
@@ -12,6 +12,24 @@ class ContactController extends AbstractController {
         if($request->checkValidity() === false){
             return new Response('Requete invalide. Doit retourner un email, un sujet et un message.',400,[]);
         }
+
+        $email = $request->getBody()["email"];
+        $domain = stristr($email, '@');
+        $extension = stristr($domain, '.');
+        $position = stripos($email, $extension);
+        $sub = substr($email, 0, $position);
+        var_dump($sub);
+
+        $jsonData = json_encode($request->getBody(), JSON_PRETTY_PRINT);
+    
+        $fichier = __DIR__ . '/../../var/contact/' . (string)time() . $sub . ".json";
+    
+        // Écrire le JSON dans un fichier
+        if (file_put_contents($fichier, $jsonData)) {
+            echo "Données enregistrées.";
+        } else {
+            echo "Erreur lors de l'enregistrement des données.";
+        }
         return new Response('Contact Controller');
     }
 }
