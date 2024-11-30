a65b729a09a1a41c684b3520b719c0bb92951f08
toto la menace <thomas.sansberro@eemi.com>
2024-11-30 11:18:27 +0100

    Ajout de Contact

---
+++ app/src/Classes/Contact.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Classes;
+
+class Contact {
+
+
+    function __construct(public string $email, public string $subject, public string $message) {
+        $this->email = $email;
+        $this->subject = $subject;
+        $this->message = $message;
+    }
+
+    function setEmail(string $email): self {
+        $this->email = $email;
+        return $this;
+    }
+
+    function getEmail(): string {
+        return $this->email;
+    }
+
+    function setSubject(string $subject): self {
+        $this->subject = $subject;
+        return $this;
+    }
+
+    function getSubject(): string {
+        return $this->subject;
+    }
+
+    function setMessage(string $message): self {
+        $this->message = $message;
+        return $this;
+    }
+
+    function getMessage(): string {
+        return $this->message;
+    }
+
+
+
+}
