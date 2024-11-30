39d3c746b6d38e22c6bafdf11b4c24042eac5a1c
toto la menace <thomas.sansberro@eemi.com>
2024-11-30 18:15:01 +0100

    déplacement fonction création fichier texte

--- app/src/Controllers/ContactController.php
+++ app/src/Controllers/ContactController.php
@@ -8,28 +8,8 @@ use App\Http\Response;
 class ContactController extends AbstractController {
 
 
-    public function process(Request $request): Response {
-
-        $email = $request->getBody()["email"];
-        $domain = stristr($email, '@');
-        $extension = stristr($domain, '.');
-        $position = stripos($email, $extension);
-        $sub = substr($email, 0, $position);
-
-        $jsonData = json_encode($request->getBody(), JSON_PRETTY_PRINT);
-    
-        $fichier = __DIR__ . '/../../var/contact/' . (string)time() . $sub . ".json";
-        
-        if(!$request->checkValidity()){
-            return $response;
-        }
-        // Écrire le JSON dans un fichier
-        if (file_put_contents($fichier, $jsonData)) {
-            echo "Données enregistrées.";
-            echo "<br />";
-            
-        } else {
-            echo "Erreur lors de l'enregistrement des données.";
-        }
+    public function process(Request $request): Response {    
+        // Check si valide
+        return $request->checkValidity();
     }
 }
--- app/src/Http/Request.php
+++ app/src/Http/Request.php
@@ -31,23 +31,48 @@ class Request {
         return $this->headers;
     }
 
-    public function checkValidity(): bool{
-        //Je recupere les propriétés de mon contact
-        $requiredKeys = $this->contact->getKeys();
-
-        $requiredAltKeys = $this->contact->getAltKeys();
-        //Je recupere les propriétés de la requete
-        $keysFromRequest = array_keys($this->body);
-        if($requiredKeys === $keysFromRequest){
-            return true;
-        }elseif($requiredAltKeys === $keysFromRequest){
-            return $response = new Response('Requete invalide. Doit retourner un email, un sujet et un message.',201,$request->getHeaders());
-        }
-        return $response = new Response('Requete invalide. Doit retourner un email, un sujet et un message.',400,$request->getHeaders());
-    }
-
     public function getBody(): array {
         return $this->body;
     }
+
+    public function checkValidity(): Response{
+
+        //recuperer l'email de la requete
+        $email = $this->getBody()["email"];
+        $domain = stristr($email, '@');
+        $extension = stristr($domain, '.');
+        $position = stripos($email, $extension);
+        $sub = substr($email, 0, $position);
+
+        //envoyer dans le fichier, le body en formé comme un json
+        $jsonData = json_encode($this->getBody(), JSON_PRETTY_PRINT);
     
+        $fichier = __DIR__ . '/../../var/contact/' . (string)time() . $sub . ".json";
+
+        //Je recupere les propriétés de mon contact
+        $requiredKeys = $this->contact->getKeys();
+        $requiredAltKeys = $this->contact->getAltKeys();
+        
+        //Je recupere les propriétés de la requete
+        $keysFromRequest = array_keys($this->body);
+        
+        if($requiredKeys === $keysFromRequest){
+        // Écrire le JSON dans un fichier
+            if (file_put_contents($fichier, $jsonData)) {
+                echo "Données enregistrées.";
+                echo "<br />";   
+            } else {
+                echo "Erreur lors de l'enregistrement des données.";
+            }
+            //return success
+            return $response = new Response(file_get_contents('php://input'),200,$this->getHeaders());
+        }
+        elseif($requiredAltKeys === $keysFromRequest){
+
+            return $response = new Response(XXXX,201,$request->getHeaders());
+        
+        }
+
+        return $response = new Response('Requete invalide. Doit retourner un email, un sujet et un message.',400,$request->getHeaders());
+    }
 }
