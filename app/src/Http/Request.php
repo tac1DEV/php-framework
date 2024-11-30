0d861c409e813c5dedd168301c9699cbae313d4c
Thomas Sansberro <thomas.sansberro@eemi.com>
2024-11-30 20:32:24 +0100

    nettoyage code

--- app/src/Http/Request.php
+++ app/src/Http/Request.php
@@ -36,63 +36,6 @@ class Request {
         return $this->body;
     }
 
-    // public function checkValidity(): Response{
-
-    //     //recuperer l'email de la requete
-    //     $email = $this->getBody()["email"];
-    //     $domain = stristr($email, '@');
-    //     $extension = stristr($domain, '.');
-    //     $position = stripos($email, $extension);
-    //     $sub = substr($email, 0, $position);
-
-    //     //envoyer dans le fichier, le body en formé comme un json
-    //     $jsonData = json_encode($this->getBody(), JSON_PRETTY_PRINT);
-    
-    //     $fichier = __DIR__ . '/../../var/contact/' . (string)time() . $sub . ".json";
-
-    //     //Je recupere les propriétés de mon contact
-    //     $requiredKeys = $this->contact->getKeys();
-        
-    //     //Je recupere les propriétés de la requete
-    //     $keysFromRequest = array_keys($this->body);
-        
-    //     if($requiredKeys === $keysFromRequest){
-    //         //Vérifier qu'aucun fichier n'existe        
-    //         $path = __DIR__ . '/../../var/contact/';
-    //         //Récuperer la liste des fichiers existants
-    //         $files = array_filter(scandir($path), function($item) {
-    //             //ne pas verifier pour "." et ".."
-    //             return $item !== '.' && $item !== '..';
-    //         });
-    //         //aucun fichier existant
-    //         if(count($files) === 0){
-    //             file_put_contents($fichier, $jsonData);
-    //         }
-    //         //recuperer le nom de mon fichier
-    //         foreach ($files as $filename)
-    //         {
-    //             if(file_exists($path . $filename))
-    //             {
-    //                 //recuperer les données existantes dans $data
-    //                 $data = array();
-    //                 $data = json_decode(file_get_contents($path . $filename), true);
-    //                 // __DIR__ . '/../../var/contact/' . (string)time() . $sub . ".json"
-    //                 array_push($data, json_decode(file_get_contents('php://input'),true));
-    //             }
-    //         }
-    //         // Écrire le JSON dans un fichier
-    //         // if (file_put_contents($fichier, $jsonData)) {
-    //         //     echo "Données enregistrées.";
-    //         //     echo "<br />";   
-    //         // } else {
-    //         //     echo "Erreur lors de l'enregistrement des données.";
-    //         // }
-    //         //return success
-    //         return $response = new Response(file_get_contents('php://input'),200,$this->getHeaders());
-    //     }
-    //     return $response = new Response('Requete invalide. Doit retourner un email, un sujet et un message.',400,$request->getHeaders());
-    // }
-
     public function checkValidity(): Response {
         //recuperer l'email de la requete
         $body = $this->getBody();
